72b417c1204a919c07ccf760e1a756ca1af734ea
Daniil Popov <x6uhrox@gmail.com>
2023-07-25 23:14:08 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
+// // // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
 // })
+
+
 
   import nodePolyfills from "rollup-plugin-polyfill-node";
   const production = process.env.NODE_ENV === "production";
